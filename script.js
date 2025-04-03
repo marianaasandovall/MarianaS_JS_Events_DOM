4a43cd640b14c1c2d97a8854796d1b5814e8e5c0
unknown <mariana10sandoval@gmail.com>
2025-04-02 19:55:19 -0700

    added code into index.html & script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+    let onLoadEl = document.getElementById("onload");
+    onLoadEl.innerHTML = "<h1>I loaded cause the DOM was fully loaded </h1>"
+})
+
+let directionEl = document.getElementById("direction");
+document.addEventListener("keydown", function(e){
+    directionEl.innerText = `The Key you pressed is ${e.code}`;
+});
